5b635277e4ff6e3e1624d8a717aee062a3c5bcd8
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2023-01-18 13:41:50 +0300

    Added func that takes a param, but declares the param as alias

--- play7.sql
+++ play7.sql
@@ -3,3 +3,13 @@ BEGIN
     RETURN subtotal * 0.06;
 END
 $$ LANGUAGE plpgsql;
+
+
+CREATE FUNCTION sales_tax2(real) RETURNS real AS $$
+    DECLARE
+        subtotal ALIAS FOR $1;
+        rate CONSTANT real := 0.06;
+    BEGIN
+        RETURN subtotal * rate;
+    END
+$$ LANGUAGE plpgsql; 
