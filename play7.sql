d42fb0b8dec219880f459b5a2fbd3c7ff6222ec5
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2023-01-18 13:47:30 +0300

    Added a func with an out parameter

--- play7.sql
+++ play7.sql
@@ -13,3 +13,10 @@ CREATE FUNCTION sales_tax2(real) RETURNS real AS $$
         RETURN subtotal * rate;
     END
 $$ LANGUAGE plpgsql; 
+
+
+CREATE FUNCTION sales_tax3(subtotal real, OUT tax real) as $$
+BEGIN
+   tax := subtotal * 0.06;
+END
+$$ LANGUAGE plpgsql;
