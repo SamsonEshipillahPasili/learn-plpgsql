eeca20b2f33ca3b5adbb5ef9253a772388f55db6
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2023-01-18 10:13:30 +0300

    Created the function with two blocks

--- play5.sql
+++ play5.sql
@@ -1,13 +1,16 @@
 CREATE FUNCTION hello_world3() RETURNS text AS $$
-<<outer>>
+<<outerblock>>
 DECLARE
     greeting text := 'hey outer';
 BEGIN
-    <<inner>>
     DECLARE
         greeting text := 'hey inner';
     BEGIN
-        
+        RAISE NOTICE 'Inner greeting: %', greeting;
+        RAISE NOTICE 'Outer greeting: %', outerblock.greeting;
     END;
+
+    RAISE NOTICE 'Outer greeting: %', greeting;
+    RETURN greeting;
 END
 $$ LANGUAGE plpgsql;
